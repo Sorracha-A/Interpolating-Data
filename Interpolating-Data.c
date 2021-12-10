1f2e958ebc2b3aa47dba67cb8a42c88db7da6759
Sorracha-A <64011628@kmitl.ac.th>
2021-12-10 20:58:38 +0700

    read data from csv file and put in an array, ready to be used.

---
+++ Interpolating-Data.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+struct points_record {
+    double x_value;
+    double y_value;
+};
+int main(){
+    FILE *data_file = fopen("C:\\Users\\sinon\\Documents\\GitHub\\Interpolating-Data\\Data\\data.csv","r");
+    if (data_file==NULL){
+        perror("Unable to open the file.");
+        exit(1);
+    }
+
+    struct points_record records[100];
+    size_t count = 0;
+    for(;count<sizeof(records)/sizeof(records[0]);++count){
+        int got = fscanf(data_file,"%lf,%lf",&records[count].x_value,&records[count].y_value);
+        if (got != 2) break;
+    }
+    fclose(data_file);
+
+
+    printf("X values are: ");
+    for (int i=0;i<count;i++){
+    printf("%.2lf ",records[i].x_value);}
+
+    printf("\nY values are: ");
+    for (int i=0;i<count;i++){
+        printf("%.2lf ",records[i].y_value);}
+
+    printf("\n\nAll the coordinates are: ");
+    for (int i=0;i<count;i++) {
+        printf("(%.2lf,%.2lf) ", records[i].x_value, records[i].y_value);
+    }
+    size_t output_size = 50;
+
+
+}
+
